59d0d98d8f2901de472887be35fb94ea65b59edd
NandoVerdu <fverdu@sanantonio1.org>
2022-03-03 17:30:35 +0000

    add questions

--- src/data/trivia.js
+++ src/data/trivia.js
@@ -1,7 +1,159 @@
 export const trivia = {
   title: "Tema 7: La distribución",
   questions:
-  [
+  [ {
+    q: "Cuando decimos que la distribución es una variable del marketing a largo plazo nos referimos a que",
+    a: [
+        "Las decisiones que se toman al respecto son difícilmente modificables",
+        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación",
+        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor"
+    ],
+},
+{ 
+    q: "Cuando decimos que la distribución es una variable del marketing que repercute en el precio nos referimos a que",
+    a: [
+        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación",
+        "Las decisiones que se toman al respecto son difícilmente modificables",
+        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor"
+    ]
+},
+{
+    q: "Cuando decimos que la distribución es una variable del marketing que produce una pérdida del control nos referimos a que",
+    a: [
+        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor",
+        "Las decisiones que se toman al respecto son difícilmente modificables",
+        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación"
+    ],
+},
+{    q: "Se refiere al traslado de los productos desde el punto de fabricación",
+    a: [   "Transporte","Difusión","Diversificación","Logística" ],
+},
+{ 
+    q: "Permite que los productos lleguen hasta diferentes puntos de venta, desde un único punto de fabricación",
+    a: [ "Difusión",  "Transporte","Diversificación","Logística" ],    
+},
+{   q: "Transformación de lotes de venta mediante división de la producción en cantidades adecuadas para su venta",
+    a:[  "Diversificación", "Transporte","Difusión","Logística" ], 
+},
+{   q: "Estudia cómo llevar el producto al público en las mejores condiciones",
+    a:[  "Logística" ,"Diversificación", "Transporte","Difusión"],    
+},
+{ 
+    q: "Los servicios, ¿son susceptibles de distribución física?",
+    a: ["No", "Sí" ],
+    followup: " La distribución física hace referencia al conjunto de actividades que conllevan una acción física sobre los productos compuesta por transporte, diversificación y almacenamiento"},
+
+{ 
+    q: "Un canal de distribución es ...",
+    a: [
+       "El camino que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino",
+         "El proceso que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino",
+        "La frecuencia que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino"
+    ],
+},
+{   q: "Según el tipo de producto que se comercializa distinguimos ...",
+    a: [ "Canales de productos de consumo, industriales y servicios",
+            "Canales de productos de consumo, industriales y de capital",
+           "Canales de productos de materias primas, suministros y servicios"
+    ],  
+},
+{   q: "Un canal de distribución directo ...",
+    a: ["No tiene intermediarios; el producto va directamente del fabricante al consumidor final",
+       "No tiene intermediarios; el producto va directamente del detallista al consumidor final",
+      "El consumidor compra directamente al mayorista"
+    ],      
+},
+{    q: "Un canal de distribución indirecto corto ...",
+    a: [
+        "Participa un único intermediario, el detallista o minorista",
+       "Participa un único intermediario, el mayorista",
+        "Participa un único intermediario, el agente"  ],
+},
+
+{ 
+    q: "Un canal de distribución indirecto largo ...",
+    a: [
+        "Participa al menos dos intermediarios, mayorista y minorista",
+       "Participa al menos dos intermediarios, mayorista y agente",
+          "Participa al menos dos intermediarios, agente y minorista"
+    ],      
+},
+
+{ 
+  q: "Un canal de distribución indirecto muy largo ...",
+  a: ["Tiene la estructura Fabricante -> Agente -> Mayorista -> Minorista -> Consumidor final",
+      "Las distancias recorridas superan los 500 km",
+      "Existen al menos dos mayoristas"
+  ],   
+},
+{   q: "El objetivo final de la estrategia de omnicanalidad es mejorar la experiencia del usuario brindándole la misma información a través de todos los canales de distribución. No es ejemplo de omnicanalidad",
+  a: [  "El cliente tiene múltiples canales de compra (web, aplicación móvil, tienda física, ...) pero no comparten los mismos datos del cliente",
+    "El cliente tiene el mismo carro de la compra en la página web y en la aplicación móvil",
+         "La empresa ofrece las mismas promociones en todos los canales",
+         "La empresa tiene las bases de datos integradas y coherentes: las unidades físicas en la aplicación móvil coinciden en la página web"
+  ],
+ },
+
+{q: "Es preferible un canal corto si hay pocos compradores",
+a: ["Verdadero", "Falso"],
+},
+
+{q: "Para el comercio internacional resulta más rentable un canal largo o muy largo, con operadores especializados",
+a: ["Verdadero", "Falso"],
+},
+
+{q:"Las limitaciones legales no afectan a la elección del canal de distribución", 
+a: ["Falso","Verdadero"],  
+},
+
+{q: "Cuanto más amplia sea la cartera de productos es más recomendable el canal directo",
+a: ["Verdadero","Falso"],
+},
+
+{q:"La distribución indirecta incurre en unos costes fijos mayores que la distribución directa",
+a:["Falso","Falso"],},
+
+{q:"La distribución indirecta incurre en unos costes variables mayores que la distribución directa",
+a:["Verdadero","Falso"],},
+
+{q: "Un inconveniente de la distribución indirecta para el fabricante es que ...",
+a: ["el precio final se ve incrementado, ya que el intermediario/s añaden un margen por su gestión",    
+  "reduce el número de transacciones con el consumidor final",
+  "reduce sus costes, ya que no se ocupa del transporte y del almacenamiento"],
+},
+
+{ 
+  q: "Adquieren mercancía a fabricantes o a otros mayoristas para revender a otros mayoristas o minoristas",
+  a: ["Mayoristas","Minoristas" ],   
+},
+{
+  q: "Venden directamente al consumidor final",
+  a:["Minoristas","Mayoristas" ], 
+},
+   
+
+{ 
+  q: "Un comercio independiente es el que ...",
+  a: [    "realiza su actividad de manera independiente",
+         "se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",
+           "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia"],    
+}
+,
+{ 
+  q: "Un comercio asociado es el que ...",
+  a: ["se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",
+     "realiza su actividad de manera independiente",
+      "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia"],  
+},
+
+{ 
+  q: "Un comercio integrado es el que ...",
+  a: [
+      "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia",
+        "realiza su actividad de manera independiente",
+         "se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",], 
+},
+
     {
       q: "Estrategia de distribución centrada en la cobertura del mercado  con la que se pretende llegar a todo el mercado, colocando el producto en el máximo número de puntos de venta posible",
       a: ["Intensiva", "Selectiva", "Exclusiva"],
@@ -51,7 +203,7 @@ export const trivia = {
       a: [ "Estrategia horizontal","Estrategia vertical"],
     
     },
-    {q: "¿Qué no es el merchandising",
+    {q: "¿Qué no es el merchandising?",
      a: ["Promociones y descuentos para los clientes fieles", "Es como un vendedor que trabajase las 24 horas del días todos los días del año haciendo una presentación activa de los productos",
     "Un conjunto de métodos y técnicas cuyo objetivo consiste en obtener la mayor rentabilidad posible del punto de venta","Una de las principales funciones del merchandising consiste en encargarse de la disposición y presentación de los productos en la tienda",
   "Incluye actividades como el emplazamiento del punto de venta y elementos externos como la fachada, escaparates, carteles"],
