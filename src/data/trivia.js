3cb8b2d5ef5df73c769da2b09431f2b5b1316f9c
fverducaballero <fverducaballero@gmail.com>
2022-03-03 09:55:20 +0100

    mistakes

--- src/data/trivia.js
+++ src/data/trivia.js
@@ -9,12 +9,12 @@ export const trivia = {
     },
     {
       q: "Estrategia de distribución centrada en la cobertura del mercado con la que se pretende llegar a un determinado número de segmentos de consumidores; limitada a un número reducido de puntos de venta ",
-      a: ["Intensiva", "Selectiva", "Exclusiva"],
+      a: ["Selectiva","Intensiva",  "Exclusiva"],
       followup: "Por ejemplo, productos cosméticos que solo se venden en farmacias",
     },
     {
       q: "Estrategia de distribucion centrada en la cobertura del mercado que se dirige a un segmento concreto",
-      a: ["Intensiva", "Selectiva", "Exclusiva"],
+      a: [ "Exclusiva","Intensiva", "Selectiva"],
       followup: "Por ejemplo, Paul and Shark solo se vende en el Corte Inglés"
     },
     {
@@ -24,13 +24,13 @@ export const trivia = {
     },
     {
       q: "Estrategia de distribución basada en la relacion fabricante-red de venta en la que la empresa utiliza un canal de distribución con intermediarios (corto, largo o muy largo)",
-      a: ["Distribución por cuenta propia", "Distribución por cuenta ajena", "Distribución mixta"],
+      a: ["Distribución por cuenta ajena","Distribución por cuenta propia",  "Distribución mixta"],
       followup:
         "Reduce los costes que implica la creación de un canal propio de distribución, como por ejemplo productos de consumo",
     },
     {
       q: "Estrategia de distribución basada en la relación fabricante-red de venta que usa una red propia de distribución allí donde es rentable y una distribución por cuenta ajena donde no resulta rentable hacerlo por cuenta propia",
-      a: ["Distribución por cuenta propia", "Distribución por cuenta ajena", "Distribución mixta"],
+      a: ["Distribución mixta","Distribución por cuenta propia", "Distribución por cuenta ajena"],
     },
     {
       q: "Estrategia de distribución que centra los esfuerzos de comunicación del fabricante sobre el distribuidor y hace presión sobre este, para que se involucre en las ventas",
@@ -39,7 +39,7 @@ export const trivia = {
     },
     {
       q: "Estrategia de distribución que centra los esfuerzos de comunicación del fabricante sobre el consumidor final para que tire del producto exigiendo al distribuidor sus marcas preferidas",
-      a: ["Estrategia push", "Estrategia pull"],
+      a: [ "Estrategia pull","Estrategia push"],
       followup: "Por ejemplo, mediante campañas publicitarias",
     },
     {
@@ -48,7 +48,7 @@ export const trivia = {
     },
     {
       q: "Estrategia de distribución en el que dos o más empresas del mismo nivel (fabricante, mayorista, minorista) colaboran persiguiendo una mayor poder de negociación realizando actividades conjuntamente",
-      a: ["Estrategia vertical", "Estrategia horizontal"],
+      a: [ "Estrategia horizontal","Estrategia vertical"],
     
     },
     {q: "¿Qué no es el merchandising",
@@ -61,14 +61,14 @@ export const trivia = {
      a: ["Disposición libre","Disposición en espiga","Disposición en parrilla","Disposición sesgada"],},
 
      {q:"Disposición en la que las estanterías se disponen oblicuamente, obligando al público a hacer el recorrido que desea el comercio, lo que favorece la compra por impulso",
-     a:["Disposición libre","Disposición en espiga","Disposición en parrilla","Disposición sesgada"],},
+     a:["Disposición en espiga","Disposición libre","Disposición en parrilla","Disposición sesgada"],},
 
      {q: "Disposición en la que las estanterías se colocan en paralelo a la circulación de la clientela",
-      a: ["Disposición libre","Disposición en espiga","Disposición en parrilla","Disposición sesgada"],
+      a: ["Disposición en parrilla","Disposición libre","Disposición en espiga","Disposición sesgada"],
     followup: "Guía al cliente hacia puntos concretos y aprovecha el espacio del comercio"},
 
       {q: "Disposición que combina la disposición de parrilla y la disposición en espiga",
-        a:["Disposición libre","Disposición en espiga","Disposición en parrilla","Disposición sesgada"],},
+        a:["Disposición sesgada","Disposición libre","Disposición en espiga","Disposición en parrilla"],},
 
   
   ]
