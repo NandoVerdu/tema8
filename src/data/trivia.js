9907bd8fd453d9067b69d83840b953e3e10a2a5f
NandoVerdu <fverdu@sanantonio1.org>
2022-03-21 16:07:32 +0000

    build

--- src/data/trivia.js
+++ src/data/trivia.js
@@ -2,226 +2,263 @@ export const trivia = {
   title: "Tema 8: La Política de Comunicación",
   questions:
   [ {
-    q: "Cuando decimos que la distribución es una variable del marketing a largo plazo nos referimos a que",
-    a: [
-        "Las decisiones que se toman al respecto son difícilmente modificables",
-        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación",
-        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor"
+    q: "El plan de comunicación es la materialización de la variable del marketing comunicación y",
+    a: ["Las otras dos son verdaderas",
+        "Determina los objetivos de comunicación de la empresa, selecciona el público objetivo, establece las acciones que se llevarán a cabo y las herramientas que se utilizarán", 
+        "Define un calendario o cronograma, presupuesta cada una de las acciones y las herramientas necesarias y controla y hace un seguimiento del plan de comunicación"
+        
     ],
 },
+{
+  q: "El mix de comunicación esta formado por",
+  a: [
+        "Publicidad, Relaciones públicas, Promoción de ventas, Venta personal y Marketing directo",
+        "Publicidad, Propaganda y Fidelización",
+        "Emisor, Receptor, mensaje, canal y código"
+    ]
+},
+
 { 
-    q: "Cuando decimos que la distribución es una variable del marketing que repercute en el precio nos referimos a que",
+    q: "Herramienta impersonal de comunicación que utiliza medios masivos para hacer llegar los mensajes a un conjunto numeroso de personas con el fin de influir en la compra o la aceptación del producto",
     a: [
-        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación",
-        "Las decisiones que se toman al respecto son difícilmente modificables",
-        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor"
+        "Publicidad",
+        "Relaciones públicas",
+        "Promoción de ventas",
+        "Venta personal",
+        "Marketing directo"
     ]
 },
 {
-    q: "Cuando decimos que la distribución es una variable del marketing que produce una pérdida del control nos referimos a que",
+    q: "Acciones comunicativas destinadas a crear una imagen positiva de la empresa o de sus productos",
     a: [
-        "Los productos acabados se entregan al distribuidor para su acercamiento al consumidor, por lo que quedan en posesión del distribuidor",
-        "Las decisiones que se toman al respecto son difícilmente modificables",
-        "Acercar el producto a la clientela conlleva costes adicionales a la fabricación"
+      "Relaciones públicas",
+      "Publicidad",
+      "Promoción de ventas",
+      "Venta personal",
+      "Marketing directo"
     ],
 },
-{    q: "Se refiere al traslado de los productos desde el punto de fabricación",
-    a: [   "Transporte","Difusión","Diversificación","Logística" ],
+{    q: "Incentivos materiales o económicos que emplea la empresa para estimular la compra a corto plazo.",
+    a: [   
+      "Promoción de ventas",
+      "Relaciones públicas",
+      "Publicidad",
+      "Venta personal",
+      "Marketing directo"
+     ],
 },
 { 
-    q: "Permite que los productos lleguen hasta diferentes puntos de venta, desde un único punto de fabricación",
-    a: [ "Difusión",  "Transporte","Diversificación","Logística" ],    
+    q: "Labor llevada a cabo por el personal de ventas para informar o persuadir a la clientela con el fin de que adquiera un determinado producto. Es un método directo de comunicación, permite asesoramiento, orientación y resolución de dudas",
+    a: [ 
+      "Venta personal",
+      "Promoción de ventas",
+      "Relaciones públicas",
+      "Publicidad",
+      "Marketing directo"
+     ],    
 },
-{   q: "Transformación de lotes de venta mediante división de la producción en cantidades adecuadas para su venta",
-    a:[  "Diversificación", "Transporte","Difusión","Logística" ], 
+{   q: "Forma de comunicación interactiva mediante uno o más medios de comunicación masivos en la que se busca la respuesta inmediata del cliente potencial.",
+    a:[  
+      "Marketing directo",
+      "Venta personal",
+    "Promoción de ventas",
+    "Relaciones públicas",
+    "Publicidad"
+    ], 
 },
-{   q: "Estudia cómo llevar el producto al público en las mejores condiciones",
-    a:[  "Logística" ,"Diversificación", "Transporte","Difusión"],    
-},
+{   q: "Las empresa deben utilizar las diferentes herramientas del marketing de manera aislada ya que si se utilizan de manera combinada el resultado puede no ser el óptimo",
+    a:[  "Falso" ,"Verdadero"],    
+}, 
+//3.La publicidad
 { 
-    q: "Los servicios, ¿son susceptibles de distribución física?",
-    a: ["No", "Sí" ],
-    followup: " La distribución física hace referencia al conjunto de actividades que conllevan una acción física sobre los productos compuesta por transporte, diversificación y almacenamiento"},
+    q: "Cuando decimos que la publicidad es una herramienta de comunicación unilateral nos referimos a que",
+    a: ["El mensaje sale únicamente del anunciante sin permitir la respuesta del público",
+    "Se dirige a un conjunto global de individuos anónimos",
+    "Se destina a un público muy amplio a través de medios de comunicación masivos" ],
+    },
 
 { 
-    q: "Un canal de distribución es ...",
-    a: [
-       "El camino que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino",
-         "El proceso que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino",
-        "La frecuencia que sigue un producto desde el punto de fabricación hasta su consumo; está formado por el conjunto de personas u organizaciones (intermediarios) que intervienen a lo largo de dicho camino"
+    q: "Cuando decimos que la publicidad es una herramienta de comunicación impersonal nos referimos a que",
+    a: ["Se dirige a un conjunto global de individuos anónimos",
+         "El mensaje sale únicamente del anunciante sin permitir la respuesta del público",
+        "Se destina a un público muy amplio a través de medios de comunicación masivos; no obstante, el grupo al que se dirige reune unas características concretas con las que identificar el producto"
     ],
 },
-{   q: "Según el tipo de producto que se comercializa distinguimos ...",
-    a: [ "Canales de productos de consumo, industriales y servicios",
-            "Canales de productos de consumo, industriales y de capital",
-           "Canales de productos de materias primas, suministros y servicios"
-    ],  
-},
-{   q: "Un canal de distribución directo ...",
-    a: ["No tiene intermediarios; el producto va directamente del fabricante al consumidor final",
-       "No tiene intermediarios; el producto va directamente del detallista al consumidor final",
-      "El consumidor compra directamente al mayorista"
+{   q: "Cuando decimos que la publicidad es una herramienta de comunicación masiva nos referimos a que",
+a: [
+  "Se destina a un público muy amplio a través de medios de comunicación masivos",
+  "Se dirige a un conjunto global de individuos anónimos",
+     "El mensaje sale únicamente del anunciante sin permitir la respuesta del público",    
+],  
+}
+/*,
+
+{   q: "",
+    a: ["",
+       "",
+      ""
     ],      
 },
-{    q: "Un canal de distribución indirecto corto ...",
+{    q: "",
     a: [
-        "Participa un único intermediario, el detallista o minorista",
-       "Participa un único intermediario, el mayorista",
-        "Participa un único intermediario, el agente"  ],
+        "",
+       "",
+        ""  ],
 },
 
 { 
-    q: "Un canal de distribución indirecto largo ...",
+    q: "",
     a: [
-        "Participa al menos dos intermediarios, mayorista y minorista",
-       "Participa al menos dos intermediarios, mayorista y agente",
-          "Participa al menos dos intermediarios, agente y minorista"
+        "",
+       "",
+          ""
     ],      
 },
 
 { 
-  q: "Un canal de distribución indirecto muy largo ...",
-  a: ["Tiene la estructura Fabricante -> Agente -> Mayorista -> Minorista -> Consumidor final",
-      "Las distancias recorridas superan los 500 km",
-      "Existen al menos dos mayoristas"
+  q: "",
+  a: ["",
+      "",
+      ""
   ],   
 },
-{   q: "El objetivo final de la estrategia de omnicanalidad es mejorar la experiencia del usuario brindándole la misma información a través de todos los canales de distribución. No es ejemplo de omnicanalidad",
-  a: [  "El cliente tiene múltiples canales de compra (web, aplicación móvil, tienda física, ...) pero no comparten los mismos datos del cliente",
-    "El cliente tiene el mismo carro de la compra en la página web y en la aplicación móvil",
-         "La empresa ofrece las mismas promociones en todos los canales",
-         "La empresa tiene las bases de datos integradas y coherentes: las unidades físicas en la aplicación móvil coinciden en la página web"
+{   q: "",
+  a: [  "",
+    "",
+         "",
+         ""
   ],
  },
 
-{q: "Es preferible un canal corto si hay pocos compradores",
-a: ["Verdadero", "Falso"],
+{q: "",
+a: ["", ""],
 },
 
-{q: "Para el comercio internacional resulta más rentable un canal largo o muy largo, con operadores especializados",
-a: ["Verdadero", "Falso"],
+{q: "",
+a: ["", ""],
 },
 
-{q:"Las limitaciones legales no afectan a la elección del canal de distribución", 
-a: ["Falso","Verdadero"],  
+{q:"", 
+a: ["",""],  
 },
 
-{q: "Cuanto más amplia sea la cartera de productos es más recomendable el canal directo",
-a: ["Verdadero","Falso"],
+{q: "",
+a: ["",""],
 },
 
-{q:"La distribución indirecta incurre en unos costes fijos mayores que la distribución directa",
-a:["Falso","Verdadero"],},
+{q:"",
+a:["",""],},
 
-{q:"La distribución indirecta incurre en unos costes variables mayores que la distribución directa",
-a:["Verdadero","Falso"],},
+{q:"",
+a:["",""],},
 
-{q: "Un inconveniente de la distribución indirecta para el fabricante es que ...",
-a: ["el precio final se ve incrementado, ya que el intermediario/s añaden un margen por su gestión",    
-  "reduce el número de transacciones con el consumidor final",
-  "reduce sus costes, ya que no se ocupa del transporte y del almacenamiento"],
+{q: "",
+a: ["",    
+  "",
+  ""],
 },
 
 { 
-  q: "Adquieren mercancía a fabricantes o a otros mayoristas para revender a otros mayoristas o minoristas",
-  a: ["Mayoristas","Minoristas" ],   
+  q: "",
+  a: ["","" ],   
 },
 {
-  q: "Venden directamente al consumidor final",
-  a:["Minoristas","Mayoristas" ], 
+  q: "",
+  a:["","" ], 
 },
    
 
 { 
-  q: "Un comercio independiente es el que ...",
-  a: [    "realiza su actividad de manera independiente",
-         "se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",
-           "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia"],    
+  q: "",
+  a: [    "",
+         "",
+           ""],    
 }
 ,
 { 
-  q: "Un comercio asociado es el que ...",
-  a: ["se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",
-     "realiza su actividad de manera independiente",
-      "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia"],  
+  q: "",
+  a: ["",
+     "",
+      ""],  
 },
 
 { 
-  q: "Un comercio integrado es el que ...",
+  q: "",
   a: [
-      "varios eslabones del canal de distribución se agrupan y forman una sola entidad de manera que cada eslabón pierde la independencia",
-        "realiza su actividad de manera independiente",
-         "se unen varios intermediarios pero conservando su independencia con el objetivo de obtener mejores precios comprando un mayor volumen o acceder con menores riesgos a nuevos mercados",], 
+      "",
+        "",
+         "",], 
 },
 
     {
-      q: "Estrategia de distribución centrada en la cobertura del mercado  con la que se pretende llegar a todo el mercado, colocando el producto en el máximo número de puntos de venta posible",
-      a: ["Intensiva", "Selectiva", "Exclusiva"],
-      followup: "Por ejemplo, productos de consumo habitual como pan, refrescos o yogures"
+      q: "",
+      a: ["", "", ""],
+      followup: ""
     },
     {
-      q: "Estrategia de distribución centrada en la cobertura del mercado con la que se pretende llegar a un determinado número de segmentos de consumidores; limitada a un número reducido de puntos de venta ",
-      a: ["Selectiva","Intensiva",  "Exclusiva"],
-      followup: "Por ejemplo, productos cosméticos que solo se venden en farmacias",
+      q: "",
+      a: ["","",  ""],
+      followup: "",
     },
     {
-      q: "Estrategia de distribucion centrada en la cobertura del mercado que se dirige a un segmento concreto",
-      a: [ "Exclusiva","Intensiva", "Selectiva"],
-      followup: "Por ejemplo, Paul and Shark solo se vende en el Corte Inglés"
+      q: "",
+      a: [ "","", ""],
+      followup: ""
     },
     {
-      q: "Estrategia de distribución basada en la relación fabricante-red de venta en la que la empresa no usa intermediarios, sino que desarrolla sus propios canales de distribución",
-      a: ["Distribución por cuenta propia", "Distribución por cuenta ajena","Distribución mixta"],
-      followup: "El fabricante realiza más funciones relacionadas con la distribución pero a cambio mantiene el control sobre el producto.",
+      q: "",
+      a: ["", "",""],
+      followup: "",
     },
     {
-      q: "Estrategia de distribución basada en la relacion fabricante-red de venta en la que la empresa utiliza un canal de distribución con intermediarios (corto, largo o muy largo)",
-      a: ["Distribución por cuenta ajena","Distribución por cuenta propia",  "Distribución mixta"],
+      q: "",
+      a: ["","",  ""],
       followup:
-        "Reduce los costes que implica la creación de un canal propio de distribución, como por ejemplo productos de consumo",
+        "",
     },
     {
-      q: "Estrategia de distribución basada en la relación fabricante-red de venta que usa una red propia de distribución allí donde es rentable y una distribución por cuenta ajena donde no resulta rentable hacerlo por cuenta propia",
-      a: ["Distribución mixta","Distribución por cuenta propia", "Distribución por cuenta ajena"],
+      q: "",
+      a: ["","", ""],
     },
     {
-      q: "Estrategia de distribución que centra los esfuerzos de comunicación del fabricante sobre el distribuidor y hace presión sobre este, para que se involucre en las ventas",
-      a: ["Estrategia push", "Estrategia pull"],
-      followup: "Por ejemplo, una oferta 2x1 al distribuidor para que tenga mayor stock y se esfuerce más a la hora de venderlo"
+      q: "",
+      a: ["", ""],
+      followup: ""
     },
     {
-      q: "Estrategia de distribución que centra los esfuerzos de comunicación del fabricante sobre el consumidor final para que tire del producto exigiendo al distribuidor sus marcas preferidas",
-      a: [ "Estrategia pull","Estrategia push"],
-      followup: "Por ejemplo, mediante campañas publicitarias",
+      q: "",
+      a: [ "",""],
+      followup: "",
     },
     {
-      q: "Estrategia de distribución que relaciona tres niveles diferentes: fabricantes, mayoristas y minoristas consiguiendo una mayor coordinación entre ellos",
-      a: ["Estrategia vertical", "Estrategia horizontal"],
+      q: "",
+      a: ["", ""],
     },
     {
-      q: "Estrategia de distribución en el que dos o más empresas del mismo nivel (fabricante, mayorista, minorista) colaboran persiguiendo una mayor poder de negociación realizando actividades conjuntamente",
-      a: [ "Estrategia horizontal","Estrategia vertical"],
+      q: "",
+      a: [ "",""],
     
     },
-    {q: "¿Qué no es el merchandising?",
-     a: ["Promociones y descuentos para los clientes fieles", "Es como un vendedor que trabajase las 24 horas del días todos los días del año haciendo una presentación activa de los productos",
-    "Un conjunto de métodos y técnicas cuyo objetivo consiste en obtener la mayor rentabilidad posible del punto de venta","Una de las principales funciones del merchandising consiste en encargarse de la disposición y presentación de los productos en la tienda",
-  "Incluye actividades como el emplazamiento del punto de venta y elementos externos como la fachada, escaparates, carteles"],
+    {q: "",
+     a: ["", "",
+    "","",
+  ""],
   }
     ,
-    {q: "Disposición en la que se ponen estanterías de distintos tipos, repartidas de forma irregular, para que parezca que se compone de varias tiendas distintas",
-     a: ["Disposición libre","Disposición en espiga","Disposición en parrilla","Disposición sesgada"],},
+    {q: "",
+     a: ["","","",""],},
 
-     {q:"Disposición en la que las estanterías se disponen oblicuamente, obligando al público a hacer el recorrido que desea el comercio, lo que favorece la compra por impulso",
-     a:["Disposición en espiga","Disposición libre","Disposición en parrilla","Disposición sesgada"],},
+     {q:"",
+     a:["","","",""],},
 
-     {q: "Disposición en la que las estanterías se colocan en paralelo a la circulación de la clientela",
-      a: ["Disposición en parrilla","Disposición libre","Disposición en espiga","Disposición sesgada"],
-    followup: "Guía al cliente hacia puntos concretos y aprovecha el espacio del comercio"},
+     {q: "",
+      a: ["","","",""],
+    followup: ""},
 
-      {q: "Disposición que combina la disposición de parrilla y la disposición en espiga",
-        a:["Disposición sesgada","Disposición libre","Disposición en espiga","Disposición en parrilla"],},
+      {q: "",
+        a:["","","",""],}, 
 
   
-  ]
+*/  ]
+
 }
